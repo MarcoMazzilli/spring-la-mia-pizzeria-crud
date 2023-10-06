c37349728bfe7216495d4bc5504b4ca9398f173d
MarcoMazzilli <mazzillimarco97@gmail.com>
2023-10-06 15:48:55 +0200

    [Add] Template to create new pizza + route on controller

--- src/main/java/org/java/controller/ControllerMain.java
+++ src/main/java/org/java/controller/ControllerMain.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -46,6 +47,14 @@ public class ControllerMain {
 		model.addAttribute("pizza", pizza);
 		
 		return "pizza/show";
+	}
+	
+	@GetMapping("pizza/create")
+	public String create(Model model) {
+		 
+		model.addAttribute("pizza" , new Pizza());
+		
+		return "pizza/create";
 	}
 
 }
